3c7bf1aa123350138a5753a9878c282e1521a2c2
lk23286 <lk23286@gmail.com>
2023-05-09 21:44:45 +0200

    Initial Commit

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by Laszlo Kovacs on 2023. 05. 09..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Laszlo Kovacs on 2023. 05. 09..
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
